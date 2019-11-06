135bf35109fce1b643d5a2cae389703edaf939bc
JaybeeGeek <johnboscoobiejesi@gmail.com>
2019-11-07 00:27:43 +0100

    error fixed

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h3>About me</h3>
+            <p>Loading........</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/pages/hire.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hire() {
+    return (
+        <div>
+            <h3>Hire Me</h3>
+            <p>Loading..........</p>
+        </div>
+    )
+}
+
+export default Hire;
