1cc36c91b38dcfd4b661f376b1b6737b0a661ef1
JaybeeGeek <johnboscoobiejesi@gmail.com>
2019-12-12 10:36:10 +0100

    update

--- src/pages/hire.js
+++ src/pages/hire.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Col } from 'react-bootstrap';
 import { Link } from 'gatsby';
 import Subscribe from '../components/mailChimpEmail';
 
@@ -15,21 +15,33 @@ function Hire() {
             <div style={{ margin: 'auto', maxWidth: '500px' }}>
                 <h2 className="text-center text-white">Hire/Contact me</h2>
                 <p className="text-center">Want to ask for something, work, business or anything. Please reach out </p>
-                <Form className="shadow-lg p-5 mb-5 bg-white rounded">
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
+                <Form action="POST" data-netlify="true" className="shadow-lg p-5 mb-5 bg-white rounded">
+                    <Form.Group>
+                        <Form.Row>
+                            <Col>
+                                <Form.Control name="name" placeholder="First name" />
+                            </Col>
+                            <Col>
+                                <Form.Control name="name" placeholder="Last name" />
+                            </Col>
+                        </Form.Row>
                     </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control type="email" name="email" placeholder="Enter email" />
                     </Form.Group>
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Send your message right here and i will get back</Form.Label>
+                        <Form.Control as="textarea" name="message" id="message" rows="4" placeholder="short me this message.  ):" />
+                    </Form.Group>
+
+                    <Form.Group id="formGridCheckbox">
+                        <Form.Check type="checkbox" label="I'm not a robot" data-netlify-recaptcha="true" />
+                    </Form.Group>
+
                     <Button variant="outline-dark" type="submit" className="text-center">
-                        Submit
+                        <i className="fa fa-paper-plane"></i> Send
                     </Button>
                 </Form>
             </div>
